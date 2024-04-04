51d2bd513b3916d8c821088158c102a4dfa29168
Gabriella Marusyak <slipuhinag@gmail.com>
2024-04-04 16:18:47 +0300

    Fixed deploy mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 // import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Route, Routes } from 'react-router-dom';
 
 import FirstPage from './pages/FirstPage/FirstPage'
 import SecondPage from './pages/SecondPage/SecondPage'
-import { Header } from './components/Header/Header'
+
 
 // import './App.css'
 
